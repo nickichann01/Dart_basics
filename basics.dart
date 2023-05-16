9c9c4cb1f45b4f8e52b85787172c0ac765e86d9b
Nyx <93776264+nickichann01@users.noreply.github.com>
2023-05-17 01:53:49 +0800

    Create basics.dart

---
+++ basics.dart
@@ -0,0 +1,51 @@
+void main() {
+
+  //Numbers
+  int num1 = 1;                   
+  double num2 = 1.5;
+  
+  //String
+  String str = 'This is Dart Language!';
+  
+  //Boolean
+  int x = 5;
+  int y = 10;
+  
+  bool isEqual = x == y;
+  bool isNotEqual = x != y;
+  bool isGreaterThan = x > y;
+  bool isLessThan = x < y;
+
+  //Lists
+  List<String> names = ['Nicki', 'Ichiko', 'Nichi'];
+  
+  //Map
+  Map shot = Map(); 
+  shot['Ikaw '] = 'Ako'; 
+  shot['Kanin '] = 'Ulam'; 
+  shot['1 '] = '2'; 
+  
+  print("-- Numbers --");
+  print(num1);
+  print(num2);
+  print(" ");
+  
+  print("-- String --");
+  print(str);
+  print(" ");
+  
+  print("-- Boolean --");
+  print(isEqual);
+  print(isNotEqual);
+  print(isGreaterThan);
+  print(isLessThan);
+  print(" ");
+
+  print("-- List --");
+  print(names);
+  print(" ");
+  
+  print("-- Map --");
+  print(shot);
+  print(" ");
+}
